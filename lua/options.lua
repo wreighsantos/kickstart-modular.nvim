5cd9c6f4e749a204568fcbcb9b7b6ce67fdefddc
Wreigh <wreighchristian.santos@infor.com>
2024-06-11 16:51:06 +0800

    nowrap

--- lua/options.lua
+++ lua/options.lua
@@ -15,6 +15,8 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.breakindent = true
 
+vim.opt.wrap = false
+
 vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 vim.opt.undofile = true
 
